11e02dcc3d9ad832dbd9703a51aa088b45b4d394
xnangz <d10858803@gmail.com>
2022-07-14 13:17:50 +0700

    penambahan soal baru

---
+++ wawasan-kebangsaan/js/questions.js
@@ -0,0 +1,212 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Planet terdekat dengan matahari Adalah ...",
+    answer: "Merkurius",
+    options: [
+      "Merkurius",
+      "venus",
+      "Jupiter",
+      "satrunus",
+      "Neptunus"
+    ]
+  },
+
+    {
+    numb: 2,
+    question: "Negara manakah yang mempunyai garis pantai terpanjang di dunia?",
+    answer: "Kanada",
+    options: [
+      "Republik Rakyat Cina",
+      "Amerika Serikat",
+      "Indonesia",
+      "Federasi Rusia",
+      "Kanada"
+    ]
+  },
+
+    {
+    numb: 3,
+    question: "ASEAN didirikan di Bangkok Thailand, pada tanggal 8 Agustus 1967 atas prakarsa 5 (lima) orang Menteri Luar Negeri dari 5 (lima) Negara. siapakah Menteri Luar Negeri Indonesia yang menjadi pemrakarsa berdirinya ASEAN?",
+    answer: "Adam Malik",
+    options: [
+      "Prof. Dr. Mochtar Kusumaatmadja, S.H., LL.M.",
+      "Adam Malik",
+      "Drs. Frans Seda",
+      "Sanusi Harjadinata",
+      "Amir Machmud" 
+    ]
+  },
+
+    {
+    numb: 4,
+    question: "Apakah makna kata kalpataru dalam bahasa sanskerta?",
+    answer: "Pohon Kehidupan",
+    options: [
+      "Pohon Kehidupan",
+      "Pohon Kemuliaan",
+      "Pohon Keabadian",
+      "Pohon Utama",
+      "Pohon Langka"
+    ]
+  },
+
+    {
+    numb: 5,
+    question: "Singkatan dari apakah LVRI yang merupakan organisasi para veteran di Indonesia?",
+    answer: "Legiun Veteran Republik Indonesia",
+    options: [
+      "Laskar Veteran Republik Indonesia",
+      "Legiun Veteran Republik Indonesia",
+      "Lembaga Veteran Republik Indonesia",
+      "Loka Veteran Republik Indonesia",
+      "Laksamana Veteran Republik Indonesia"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Kontingen Indonesia Mendapatkan medali pertama, yaitu medali perak, pada Olimpiade ke-24 tahun 1988 yang dilangsungkan di Seoul Korea Selatan. Medali tersebut didapatkan oleh atlit-atlit beregu indonesia dari cabang olahraga ...",
+    answer: "Panahan",
+    options: [
+      "Panahan",
+      "Bulu Tangkis",
+      "Dayung",
+      "Menembak",
+      "Angkat Besi"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Ibukota dari negara Libya adalah ...",
+    answer: "Tripoli",
+    options: [
+      "Kairo",
+      "Addis Ababa",
+      "N'djamena",
+      "Tripoli",
+      "Djibouti"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Lagu Cik Cik Periuk adalah lagu daerah masyarakat ...",
+    answer: "Kalimantan Barat",
+    options: [
+      "Kalimantan Timur",
+      "Kalimantan Selatan",
+      "Kalimantan Tengah",
+      "Kalimantan Barat",
+      "Sulawesi Utara"
+    ]
+  },
+  
+  {
+    numb: 9,
+    question: "Penemu Photo Copy pada tahun 1938 adalah ...",
+    answer: "Chester Floyd Carlson",
+    options: [
+      "Chester Moor Hall",
+      "Chester Floyd Carlson",
+      "Antonio Santi giuseppe Meucci",
+      "Blaise Pascal",
+      "Count Alessandro"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Salah Satu negara yang menjadi anggota organisasi G8 (Group of Eight) adalah ...",
+    answer: "Jepang",
+    options: [
+      "Jepang",
+      "Spanyol",
+      "Australia",
+      "Korea Selatan",
+      "Republik Rakyat Cina"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Bapak Pramuka dunia adalah ...",
+    answer: "Robert Baden Powel",
+    options: [
+      "Morse",
+      "Christopher Latham sholes",
+      "Pierre de Frédy, Baron de Coubertin",
+      "Robert Baden Powel",
+      "Carl Diem"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "Siapakah petinju Indonesia pertama yang berhasil meraih gelar juara dunia?",
+    answer: "Ellyas Pical",
+    options: [
+      "Wongso Suseno",
+      "Ellyas Pical",
+      "Thomas Americo",
+      "Nico Thomas",
+      "Chris John"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "Siapakah arsitek candi borobudur?",
+    answer: "Gunadharma",
+    options: [
+      "Gunadharma",
+      "Sambharabhudhara",
+      "Dyah Pancapana Rakai Panangkaran",
+      "Sri Maharaja Rakai Kayuwangi",
+      "Tulodong"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Flora Khas dari Aceh adalah ...",
+    answer: "Bunga Jeumpa",
+    options: [
+      "Bunga Kenanga",
+      "Pohon Kayu Andalas",
+      "Bunga Suweg Raksasa",
+      "Bunga Jeumpa",
+      "Bunga Rafflesia Arnoldii"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Pada pasal berapakah dari Undang-Undang Dasar 1945 yang menyatakan: Presiden memegang kekuasaan tertinggi atas Angakatan Darat, Angkatan Laut, Angkatan Udara",
+    answer: "Pasal 10",
+    options: [
+      "Pasal 8",
+      "Pasal 9",
+      "Pasal 10",
+      "Pasal 11",
+      "Pasal 12"
+    ]
+  },
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
